8ad0a4b4c16d68cbd593133624ad824a3b8e84b1
Raheem <ganjul4real@gmail.com>
2023-09-08 18:08:53 +0100

    commit

--- app.js
+++ app.js
@@ -1,11 +1,3 @@
-function display_c() {
-	var refresh = 1000; // Refresh rate in milli seconds
-	mytime = setTimeout("display_ct()", refresh);
-}
+const displayUtc = new Date().toUTCString();
 
-function display_ct() {
-	var x = new Date();
-	var x1 = x.toUTCString();
-	document.getElementById("UTC-time").textContent = x1;
-	display_c();
-}
+document.getElementById("UTCtime").innerHTML = displayUtc;
